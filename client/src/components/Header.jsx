dec39b08e52303cdd9eeedfdf961e6489f2256db
Jun Park <jun.parkc@gmail.com>
2018-01-26 18:54:19 -0500

    Navbar update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Navigation from './Navigation.jsx';
+import NavBarContainer from './navbar/NavBarContainer.jsx';
 
 class Header extends Component {
     render() {
         return (
             <Switch>
-                <Route path='/reception' component={Navigation} />
-                <Route path='/dashboard' component={Navigation} />
-                <Route path='/schedule' component={Navigation} />
-                <Route path='/settings' component={Navigation} />
-                <Route path='/customerservice' component={Navigation} />
-                <Route exact path='/' component={Navigation} />
+                <Route component={NavBarContainer} />
             </Switch>
         );
     }
