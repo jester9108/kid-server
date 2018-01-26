f0593355c43ced41ad804ced39a6fd467bff4f63
Jun Park <jun.parkc@gmail.com>
2018-01-26 18:53:57 -0500

    Update bunch of redux actions & reducers

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,23 +1,97 @@
-
-/* Action types */
+/* Modal */
 export const SHOW_MODAL = 'SHOW_MODAL';
-export const HIDE_MODAL = 'HIDE_MODAL';
-export const GET_USER = 'GET_USER';
-export const SET_USER = 'SET_USER';
-
-/* Action creators */
 export function showModal(modalType) {
     return { type: SHOW_MODAL, modalType: modalType };
 }
 
+export const HIDE_MODAL = 'HIDE_MODAL';
 export function hideModal() {
     return { type: HIDE_MODAL };
 }
 
-export function getUser() {
-    return { type: GET_USER };
+/* User login/logout */
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export function loginRequest() {
+    return { type: LOGIN_REQUEST };
 }
 
-export function setUser(user) {
-    return { type: SET_USER, user: user };
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export function loginSuccess(accessToken) {
+    return { type: LOGIN_SUCCESS, accessToken: accessToken };
+}
+
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export function loginFailure(error) {
+    return { type: LOGIN_FAILURE, error: error };
+}
+
+export const LOGOUT = 'LOGOUT';
+export function logout() {
+    return function (dispatch) {
+        dispatch({ type: LOGOUT });
+        localStorage.removeItem('accessToken');
+    };
+}
+
+export function login(email, password) {
+    return function (dispatch) {
+        dispatch(loginRequest());
+        const formBody = new URLSearchParams({
+            username: email,
+            password: password,
+            grant_type: 'password',
+            client_id: null,
+            client_secret: null,
+        });
+        return fetch('/api/store/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+            body: formBody,
+        })
+            .then(response => response.json())
+            .then((response) => {
+                if (response.access_token) {
+                    dispatch(loginSuccess(response.access_token));
+                    localStorage.setItem('accessToken', response.access_token);
+                } else {
+                    dispatch(loginFailure(response.message));
+                }
+            });
+    }
+}
+
+/* User data */
+export const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
+export function fetchUserRequest() {
+    return { type: FETCH_USER_REQUEST };
+}
+
+export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
+export function fetchUserSuccess(userData) {
+    return { type: FETCH_USER_SUCCESS, userData: userData };
+}
+
+export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
+export function fetchUserFailure(error) {
+    return { type: FETCH_USER_FAILURE, error: error };
+}
+
+export function fetchUser() {
+    return function (dispatch, getState) {
+        dispatch(fetchUserRequest());
+        const { accessToken } = getState();
+        if (!accessToken) return null;
+        return fetch('/api/store/me', {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${accessToken}` },
+        })
+            .then(response => response.json())
+            .then((response) => {
+                if (response.success) {
+                    dispatch(fetchUserSuccess(response.data));
+                } else {
+                    dispatch(fetchUserFailure(response.message));
+                }
+            });
+    }
 }
--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,57 +1,92 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
+import reduceReducers from 'reduce-reducers';
 
-import {
-    SHOW_MODAL,
-    HIDE_MODAL,
-    GET_USER,
-    SET_USER,
-} from './actions'
-
-/* Initial modal state */
-const initialModalState = {
-    modalType: null,
-};
+import * as ACTION from './actions'
 
 /* Modal reducer */
-function modalDisplay(state = initialModalState, action) {
+const initialState = {
+    modalType: null,
+    isLoading: false,
+    loginState: {
+        loggedIn: false,
+        error: null,
+    },
+    userData: null,
+    error: null,
+    accessToken: null,
+};
+function modalState(state = initialState, action) {
     switch (action.type) {
-        case SHOW_MODAL:
+        case ACTION.SHOW_MODAL:
             return Object.assign({}, state, { modalType: action.modalType });
-        case HIDE_MODAL:
-            return initialModalState;
+        case ACTION.HIDE_MODAL:
+            return Object.assign({}, state, { modalType: null });
         default:
             return state;
     }
 }
 
-/* Page reducer */
-// function page(state = null, action) {
-//     switch (action.type) {
-//         case SHOW_MODAL:
-//             return Object.assign({}.state, { modalType: action.modalType });
-//         case HIDE_MODAL:
-//             return initialModalState;
-//         default:
-//             return state;
-//     }
-// }
+/* Login reducer */
+function loginState(state = initialState, action) {
+    switch (action.type) {
+        case ACTION.LOGIN_REQUEST:
+            return Object.assign({}, state, { isLoading: true });
+        case ACTION.LOGIN_SUCCESS:
+            return Object.assign({}, state, {
+                isLoading: false,
+                loginState: { loggedIn: true, error: null },
+                accessToken: action.accessToken,
+            });
+        case ACTION.LOGIN_FAILURE:
+            return Object.assign({}, state, {
+                isLoading: false,
+                loginState: { loggedIn: false, error: action.error },
+                accessToken: null,
+            });
+        case ACTION.LOGOUT:
+            return Object.assign({}, state, {
+                isLoading: false,
+                loginState: { loggedIn: false, error: null },
+                accessToken: null,
+                userData: null,
+            });
+        default:
+            return state;
+    }
+}
 
 /* User reducer */
-function user(state = null, action) {
+function userState(state = initialState, action) {
     switch (action.type) {
-        case GET_USER:
-            return 'Playground';
-        case SET_USER:
-            return null;
+        case ACTION.FETCH_USER_REQUEST:
+            return Object.assign({}, state, { isLoading: true });
+        case ACTION.FETCH_USER_SUCCESS:
+            return Object.assign({}, state, {
+                isLoading: false,
+                userData: action.userData,
+                error: null,
+            });
+        case ACTION.FETCH_USER_FAILURE:
+            return Object.assign({}, state, {
+                isLoading: false,
+                userData: null,
+                error: action.error,
+            });
         default:
             return state;
     }
 }
 
-const kidApp = combineReducers({
-    modalDisplay,
-    user,
-    // page,
-})
+// const rootReducer = combineReducers({
+//     modalState,
+//     loginState,
+//     user,
+// })
 
-export default kidApp;
+const rootReducer = reduceReducers(
+    modalState,
+    loginState,
+    userState,
+);
+
+export default rootReducer;
